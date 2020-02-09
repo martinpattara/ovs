8f60d24c5195ce8f36f19bb3d2a1eed03378daac
Martin Varghese <martin.varghese@nokia.com>
2020-02-08 22:22:49 -0800

    Encap & Decap actions for MPLS packet type
    The encap & decap actions are extended to support MPLS packet type.
    Encap & decap actions adds and removes MPLS header at start of the
    packet.
    
    Signed-off-by: Martin Varghese <martin.varghese@nokia.com>

--- lib/odp-util.h
+++ lib/odp-util.h
@@ -290,7 +290,8 @@ enum slow_path_reason commit_odp_actions(const struct flow *,
                                          bool use_masked,
                                          bool pending_encap,
                                          bool pending_decap,
-                                         struct ofpbuf *encap_data);
+                                         struct ofpbuf *encap_data,
+                                         bool mpls_l2_support);
 
 /* ofproto-dpif interface.
  *
