a455806f082d4c9e41bb223027f0e246e89a8240
Martin Varghese <martin.varghese@nokia.com>
2020-05-21 00:07:17 -0700

    Upstream kernel changes fixed

--- datapath/linux/compat/include/net/bareudp.h
+++ datapath/linux/compat/include/net/bareudp.h
@@ -6,30 +6,6 @@
 #endif
 
 
-#ifdef USE_UPSTREAM_TUNNEL
-#include_next <net/bareudp.h>
-
-static inline int rpl_bareudp_init_module(void)
-{
-	return 0;
-}
-static inline void rpl_bareudp_cleanup_module(void)
-{}
-
-#define bareudp_xmit dev_queue_xmit
-
-#ifdef CONFIG_INET
-#ifdef HAVE_NAME_ASSIGN_TYPE
-static inline struct net_device *rpl_bareudp_dev_create(
-	struct net *net, const char *name, u8 name_assign_type, struct bareudp_conf *conf) {
-	return bareudp_dev_create(net, name,name_assign_type, conf);
-}
-#define bareudp_dev_create rpl_bareudp_dev_create
-#endif
-#endif
-
-#else
-
 struct bareudp_conf {
         __be16 ethertype;
         __be16 port;
@@ -49,7 +25,6 @@ void rpl_bareudp_cleanup_module(void);
 #define bareudp_xmit rpl_bareudp_xmit
 netdev_tx_t rpl_bareudp_xmit(struct sk_buff *skb);
 
-#endif
 #define bareudp_init_module rpl_bareudp_init_module
 #define bareudp_cleanup_module rpl_bareudp_cleanup_module
 
